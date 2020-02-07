915b3b2ee2132e751f8a2cb02f3556c9dca99ab6
Temidayo Oyedele <you@example.com>
2020-02-07 15:01:05 +0100

    make responsive

--- client/src/components/authentication/auth.jsx
+++ client/src/components/authentication/auth.jsx
@@ -6,25 +6,25 @@ function RequireAdmin(ComposedComponent){
 
   class Authenticated extends React.Component {
 
-    componentWillMount(){
-        const token = localStorage.getItem("token");
-        utils.verifyToken(token)
-        .then((logged) => {
-            console.log(logged, "logged")
-          if (!logged){
-            browserHistory.push("/signin")
-          }
-        });
-    }
-    componentWillUpdate(nextProps) {
-        console.log(nextProps, 'nextprops')
-        utils.verifyToken(localStorage.getItem("token"))
-        .then((logged) => {
-          if (!logged){
-            browserHistory.push("/signin")
-          }
-        });
-      }
+    // componentWillMount(){
+    //     const token = localStorage.getItem("token");
+    //     utils.verifyToken(token)
+    //     .then((logged) => {
+    //         console.log(logged, "logged")
+    //       if (!logged){
+    //         browserHistory.push("/signin")
+    //       }
+    //     });
+    // }
+    // componentWillUpdate(nextProps) {
+    //     console.log(nextProps, 'nextprops')
+    //     utils.verifyToken(localStorage.getItem("token"))
+    //     .then((logged) => {
+    //       if (!logged){
+    //         browserHistory.push("/signin")
+    //       }
+    //     });
+    //   }
 
     render(){
       return <ComposedComponent />;
