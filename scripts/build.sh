15cc79dbb4767a9ab7fff4e91eaa6da23a45fa60
Temidayo Oyedele <you@example.com>
2020-06-20 16:02:35 +0100

    setup ssh for github

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,13 @@ echo "this computers public ip address is $public_ip_address"
 aws ec2 authorize-security-group-ingress --region eu-west-2 --group-id sg-0a4b13c43813deb62 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 22, \"ToPort\": 22, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
 cat 3.8.150.78 >> ~/.ssh/known_hosts
 ssh -t ubuntu@3.8.150.78 <<-'ENDSSH'
-  git clone ssh://git@github.com/babadee001/hyg.git -b static
+  if [ -d hyg ]; then
+    cd hyg
+    git pull
+  else
+    git clone ssh://git@github.com/babadee001/hyg.git -b static
+    cd hyg
+  fi
   pm2 start npm -- start
 ENDSSH
 aws ec2 revoke-security-group-ingress --region eu-west-2 --group-id sg-0a4b13c43813deb62 --protocol tcp --port 22 --cidr "${public_ip_address}/32"
