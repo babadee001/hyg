f7982ec4e9696a9790eb1fb7b886bc503a2736b7
Temidayo Oyedele <you@example.com>
2020-06-20 19:26:28 +0100

    setup pm2

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,18 +3,16 @@ public_ip_address=$(wget -qO- http://checkip.amazonaws.com)
 echo 'ec2-3-8-150-78.eu-west-2.compute.amazonaws.com,3.8.150.78 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBIz0XjKb/TlssBaPEGHF3NsEft/NOWY+NAEsaknXK5ZG0dUqcOnKxMCCkrd++mtdEeIEGxgNXdz3KSMDEqddY24=' >> ~/.ssh/known_hosts
 echo "this computers public ip address is $public_ip_address"
 aws ec2 authorize-security-group-ingress --region eu-west-2 --group-id sg-0a4b13c43813deb62 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 22, \"ToPort\": 22, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
-ssh ubuntu@3.8.150.78 <<-'ENDSSH'
-  if [ -d hyg ]; then
-    cd hyg
-    git pull
-    pwd
-    npm install
-  else
-    git clone ssh://git@github.com/babadee001/hyg.git -b static
-    cd hyg
-    pwd
-    npm install
-  fi
-  /home/ubuntu/.nvm/versions/node/v14.4.0/bin/pm2 start /home/ubuntu/.nvm/versions/node/v14.4.0/bin/npm -- start
-ENDSSH
+echo "if [ -d hyg ]; then
+        cd hyg
+        git pull
+        pwd
+        npm install
+    else
+      git clone ssh://git@github.com/babadee001/hyg.git -b static
+      cd hyg
+      pwd
+      npm install
+    fi
+    /home/ubuntu/.nvm/versions/node/v14.4.0/bin/pm2 start /home/ubuntu/.nvm/versions/node/v14.4.0/bin/npm -- start" | ssh -t ubuntu@3.8.150.78
 aws ec2 revoke-security-group-ingress --region eu-west-2 --group-id sg-0a4b13c43813deb62 --protocol tcp --port 22 --cidr "${public_ip_address}/32"
