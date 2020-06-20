c595cff332a360d3e0e1e6fe6de07b809259827b
Temidayo Oyedele <you@example.com>
2020-06-20 19:46:53 +0100

    test remote access

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@ public_ip_address=$(wget -qO- http://checkip.amazonaws.com)
 echo 'ec2-3-8-150-78.eu-west-2.compute.amazonaws.com,3.8.150.78 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBIz0XjKb/TlssBaPEGHF3NsEft/NOWY+NAEsaknXK5ZG0dUqcOnKxMCCkrd++mtdEeIEGxgNXdz3KSMDEqddY24=' >> ~/.ssh/known_hosts
 echo "this computers public ip address is $public_ip_address"
 aws ec2 authorize-security-group-ingress --region eu-west-2 --group-id sg-0a4b13c43813deb62 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 22, \"ToPort\": 22, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
-ssh -t ssh -t ubuntu@3.8.150.78 <<-'ENDSSH'
+ssh -t ubuntu@3.8.150.78 <<-'ENDSSH'
   touch circle.sh
   sudo chmod +x circle.sh
   echo "echo pwd" >> circle.sh
