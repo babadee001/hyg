3b61e39ecf5d28dd4030a7d44781e07e0ae6ee88
Temidayo Oyedele <you@example.com>
2020-06-20 11:58:04 +0100

    static file

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Signup from './components/signup/signup';
-import Signin from './components/signin/signin';
-import Contact from './components/contact';
-import Reset from './components/resetPassword/index';
-import Auth from './components/authentication/auth';
-import Data from './components/data';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
- 
-  render() {
-    return (
-        <Router>
-          <div className="App">
-            <Route exact path='/' component={Signin} />
-            <Route path='/signup' component={Signup} />
-            <Route path='/signin' component={Signin} />
-            <Route path='/contact' component={Contact} />
-            <Route path='/reset' component={Reset} />
-            <Route path='/dashboard' component={Auth(Data)} />   
-        </div>
-        </Router>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
