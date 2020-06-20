81b5393bf946fcca613ce9a20902e79849404da4
Temidayo Oyedele <you@example.com>
2020-06-20 21:20:37 +0100

    test remote access

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,8 @@ echo 'ec2-3-8-150-78.eu-west-2.compute.amazonaws.com,3.8.150.78 ecdsa-sha2-nistp
 echo "this computers public ip address is $public_ip_address"
 aws ec2 authorize-security-group-ingress --region eu-west-2 --group-id sg-0a4b13c43813deb62 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 22, \"ToPort\": 22, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
 ssh -t ubuntu@3.8.150.78 <<-'ENDSSH'
+  sudo chown -R $USER /usr/local/lib/node_modules
+  sudo chown -R $USER /usr/lib/node_modules
   curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
   sudo apt-get install -y nodejs
   npm install pm2 -g
