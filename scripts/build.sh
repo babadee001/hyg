3b9ba341e284f4f6b94c4bc5c4a7d2e837663652
Temidayo Oyedele <you@example.com>
2020-06-20 15:19:35 +0100

    setup ssh permission for host

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,9 @@
 public_ip_address=$(wget -qO- http://checkip.amazonaws.com)
 echo "this computers public ip address is $public_ip_address"
 aws ec2 authorize-security-group-ingress --region eu-west-2 --group-id sg-0a4b13c43813deb62 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 22, \"ToPort\": 22, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
-ssh -o StrictHostKeyChecking=no ubuntu@3.8.150.78 -v
-git clone https://github.com/babadee001/hyg -b static
-pm2 start npm -- start
+cat 3.8.150.78 >> ~/.ssh/known_hosts
+ssh -t ubuntu@3.8.150.78 <<-'ENDSSH'
+  git clone https://github.com/babadee001/hyg -b static
+  pm2 start npm -- start
+ENDSSH
 aws ec2 revoke-security-group-ingress --region eu-west-2 --group-id sg-0a4b13c43813deb62 --protocol tcp --port 22 --cidr "${public_ip_address}/32"
