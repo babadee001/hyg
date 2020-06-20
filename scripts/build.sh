e56eca8ceecc360f9caa598dbbb4886bf3ba0b39
Temidayo Oyedele <you@example.com>
2020-06-20 21:10:11 +0100

    test remote access

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,8 @@ echo "this computers public ip address is $public_ip_address"
 aws ec2 authorize-security-group-ingress --region eu-west-2 --group-id sg-0a4b13c43813deb62 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 22, \"ToPort\": 22, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
 ssh -t ubuntu@3.8.150.78 <<-'ENDSSH'
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-  ~/.nvm/nvm.sh
-  nvm install node
+  sudo ~/.nvm/nvm.sh
+  sudo nvm install node
   npm install pm2 -g
   if [ -d hyg ]; then
     cd hyg
