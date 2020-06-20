c1707043baa6ab85a5e31e865ee25c29f2b4384d
Temidayo Oyedele <you@example.com>
2020-06-20 20:34:48 +0100

    test remote access

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,34 +4,17 @@ echo 'ec2-3-8-150-78.eu-west-2.compute.amazonaws.com,3.8.150.78 ecdsa-sha2-nistp
 echo "this computers public ip address is $public_ip_address"
 aws ec2 authorize-security-group-ingress --region eu-west-2 --group-id sg-0a4b13c43813deb62 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 22, \"ToPort\": 22, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
 ssh -t ubuntu@3.8.150.78 <<-'ENDSSH'
-  touch circle.sh
-  sudo chmod +x circle.sh
-  echo "
-  #!/bin/bash
-    if [ -d hyg ]; then
-      cd hyg
-      git pull
-      pwd
-      npm install
-    else
-      git clone ssh://git@github.com/babadee001/hyg.git -b static
-      cd hyg
-      pwd
-      npm install
-    fi
-    pm2 start npm -- start" > circle.sh
+  if [ -d hyg ]; then
+    cd hyg
+    git pull
+    pwd
+    /home/ubuntu/.nvm/versions/node/v14.4.0/bin/npm install
+  else
+    git clone ssh://git@github.com/babadee001/hyg.git -b static
+    cd hyg
+    pwd
+    /home/ubuntu/.nvm/versions/node/v14.4.0/bin/npm install
+  fi
+  /home/ubuntu/.nvm/versions/node/v14.4.0/bin/pm2 start /home/ubuntu/.nvm/versions/node/v14.4.0/bin/npm -- start
 ENDSSH
-ssh -t ubuntu@3.8.150.78 "./circle.sh"
-# echo "if [ -d hyg ]; then
-#         cd hyg
-#         git pull
-#         pwd
-#         npm install
-#     else
-#       git clone ssh://git@github.com/babadee001/hyg.git -b static
-#       cd hyg
-#       pwd
-#       npm install
-#     fi
-#     /home/ubuntu/.nvm/versions/node/v14.4.0/bin/pm2 start /home/ubuntu/.nvm/versions/node/v14.4.0/bin/npm -- start" | ssh -t ubuntu@3.8.150.78
 aws ec2 revoke-security-group-ingress --region eu-west-2 --group-id sg-0a4b13c43813deb62 --protocol tcp --port 22 --cidr "${public_ip_address}/32"
